bcd1397972260688a7b910359e41603ad6e8d306
shuvo9620 <smahfuzur0@gmail.com>
2023-04-10 16:04:09 +0600

    header section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <nav>
-            <Link to="/">WorkForce</Link>
-            <Link to="/statistics">Statistics</Link>
-            <Link to="/appliedjob">Applied Jobs</Link>
-            <Link to="/blog">Blog</Link>
-        </nav>
+        <div className='header'>
+            <div className='brand-name'>
+                <h2>WorkForce</h2>
+            </div>
+            <nav>
+                <Link to="/statistics">Statistics</Link>
+                <Link to="/appliedjob">Applied Jobs</Link>
+                <Link to="/blog">Blog</Link>
+            </nav>
+            <div>
+                <button className='apply-btn'>Start Applying</button>
+            </div>
+        </div>
     );
 };
 
