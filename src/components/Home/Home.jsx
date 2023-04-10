3b9e006d210f000f134e10af55262f049040ce6f
shuvo9620 <smahfuzur0@gmail.com>
2023-04-10 17:45:53 +0600

    banner section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className='banner-section'>
+            <div className='text-area'>
+                <div className='banner-text'>
+                    <h2>One Step <br /> Closer To Your <span className='color-text'>Dream Job</span></h2>
+                    <p>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
+                    <button className='banner-btn'>Get Started</button>
+                </div>
+            </div>
+            <div className='banner-img'>
+                <img src="All Images/man.png" alt="" />
+            </div>
         </div>
     );
 };
