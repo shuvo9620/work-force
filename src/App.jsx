68a3eaf04b76ccdaca16109b103d1de13fb506e3
shuvo9620 <smahfuzur0@gmail.com>
2023-04-12 03:59:12 +0600

    featurejobdetails design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/JobFeatureDetail/JobFeatureDetail.jsx
+++ src/components/JobFeatureDetail/JobFeatureDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { addToDb } from '../../utilities/fakedb';
+import './JobFeatureDetail.css'
 
 const JobFeaturedDetail = () => {
     const { id } = useParams();
@@ -30,12 +31,41 @@ const JobFeaturedDetail = () => {
 
     return (
         <div>
+            {/* <div className='top'>
+                <img src="/All Images/Vector.png" alt="" />
+                <img src="/public/All Images/Vector-1.png" alt="" />
+            </div> */}
             {isLoading ? (
                 <p>Loading job details...</p>
             ) : jobToDisplay ? (
                 <div>
-                    <div>
-                        <p>Job Description:{jobToDisplay.JobTitle}</p>
+                    <div className='job-header'>
+                        <h2>Job Details</h2>
+                    </div>
+                    <div className='job-details'>
+                        <div>
+                            <p><strong>Job Description:</strong> {jobToDisplay.JobDescription}</p>
+                            <p><strong>Job Responsibility:</strong> {jobToDisplay.JobResponsibility}</p>
+                            <p><strong>Educational Requirements:</strong> {jobToDisplay.EducationalRequirements}</p>
+                            <p><strong>Experiences:</strong> {jobToDisplay.Experiences}</p>
+                        </div>
+
+                        <div className='job-two card p-4 w-75'>
+                            <h3>Job Details</h3>
+                            <p><img src="/Icons/Frame.png"/>
+                            <strong>Salary: </strong>{jobToDisplay.Salary}</p>
+                            <p> <img src="/Icons/Frame-1.png"/>
+                            <strong>Job Title: </strong>{jobToDisplay.JobTitle}</p>
+                            <h3>Contact Info</h3>
+                            <p> <img src="/Icons/Frame-2.png"/>
+                            <strong>Phone: </strong>{jobToDisplay.phone}</p>
+                            <p> <img src="/Icons/Frame-3.png"/>
+                            <strong>Email: </strong>{jobToDisplay.email}</p>
+                            <p><strong> <img src="/Icons/Frame-4.png"/>
+                            Address: </strong>{jobToDisplay.Location}</p>
+                            <button onClick={() => handleApplyJob(id)} className='button-apply'>Apply job</button>
+                        </div>
+
                     </div>
                 </div>
             ) : featuredData.length > 0 ? (
